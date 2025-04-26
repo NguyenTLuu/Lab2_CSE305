42ea5f6929db17337267d423efd9b588cb0148ea
TrongTranBinh <trongtrank12eiu@gmail.com>
2025-04-26 10:02:08 +0700

    Update User, Add Property Owner and Registered User entity

--- Rental Management Application/src/User.java
+++ Rental Management Application/src/User.java
@@ -4,6 +4,38 @@ public class User {
     private String email;
     private String userType;
 
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
     public User(String userID, String password, String email, String userType) {
         this.userID = userID;
         this.password = password;
